5d233328ab3ebf655752055cec169a4024ed5467
phinner <62483793+Phinner@users.noreply.github.com>
2023-09-01 01:14:02 +0200

    fix(moderation): nsfw detection bugfixes

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/misc/BlockClusterManager.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/misc/BlockClusterManager.kt
@@ -18,27 +18,17 @@
 package com.xpdustry.imperium.mindustry.misc
 
 import java.net.InetAddress
+import java.util.LinkedList
 
 class BlockClusterManager<T : Any>(private val listener: Listener<T>) {
     val clusters: List<Cluster<T>> get() = _clusters
     private val _clusters = mutableListOf<Cluster<T>>()
 
-    fun removeElement(x: Int, y: Int) {
-        for (i in _clusters.indices) {
-            for (e in _clusters[i]._blocks) {
-                if (e.x == x && e.y == y) {
-                    _clusters.removeAt(i)._blocks.forEach { addElement(it) }
-                    return
-                }
-            }
-        }
-    }
-
     fun addElement(block: ClusterBlock<T>) {
-        val candidates = mutableListOf<Cluster<T>>()
-        for (cluster in _clusters) {
-            if (canBePartOfCluster(cluster, block)) {
-                candidates += cluster
+        val candidates = mutableListOf<Int>()
+        for (i in _clusters.indices) {
+            if (canBePartOfCluster(_clusters[i], block)) {
+                candidates += i
             }
         }
         if (candidates.isEmpty()) {
@@ -46,42 +36,120 @@ class BlockClusterManager<T : Any>(private val listener: Listener<T>) {
             cluster._blocks += block
             cluster.update()
             _clusters += cluster
-            listener.onClusterUpdate(cluster)
+            listener.onClusterEvent(cluster, Event.NEW)
         } else if (candidates.size == 1) {
-            candidates[0]._blocks += block
-            candidates[0].update()
-            listener.onClusterUpdate(candidates[0])
+            _clusters[candidates[0]]._blocks += block
+            _clusters[candidates[0]].update()
+            listener.onClusterEvent(_clusters[candidates[0]], Event.UPDATE)
         } else {
             val cluster = Cluster<T>()
-            for (candidate in candidates) {
-                cluster._blocks += candidate._blocks
-                _clusters.remove(candidate)
+            for ((shift, i) in candidates.withIndex()) {
+                val target = _clusters.removeAt(i - shift)
+                cluster._blocks += target._blocks
+                listener.onClusterEvent(target, Event.REMOVE)
             }
+            cluster._blocks += block
             cluster.update()
             _clusters += cluster
-            listener.onClusterUpdate(cluster)
+            listener.onClusterEvent(cluster, Event.NEW)
         }
+
+        reorder()
+    }
+
+    fun removeElement(x: Int, y: Int) {
+        var c = -1
+        var b = -1
+        for (i in _clusters.indices) {
+            b = _clusters[i]._blocks.indexOfFirst { it.x == x && it.y == y }
+            if (b != -1) {
+                c = i
+                break
+            }
+        }
+
+        if (c == -1) {
+            return
+        }
+
+        val blocks = LinkedList(_clusters[c]._blocks.toMutableList().apply { removeAt(b) })
+        if (blocks.isEmpty()) {
+            listener.onClusterEvent(_clusters.removeAt(c), Event.REMOVE)
+            return
+        }
+
+        val result = mutableListOf<Cluster<T>>()
+        while (blocks.isNotEmpty()) {
+            val cluster = Cluster(listOf(blocks.pop()))
+            var added: Boolean
+            do {
+                added = false
+                for (i in blocks.indices) {
+                    if (canBePartOfCluster(cluster, blocks[i])) {
+                        cluster._blocks += blocks.removeAt(i)
+                        added = true
+                        break
+                    }
+                }
+            } while (blocks.isNotEmpty() && added)
+            result += cluster
+        }
+
+        if (result.size == 1) {
+            _clusters[c] = result[0]
+            _clusters[c].update()
+            reorder()
+            listener.onClusterEvent(_clusters[c], Event.UPDATE)
+            return
+        }
+
+        listener.onClusterEvent(_clusters.removeAt(c), Event.REMOVE)
+        for (cluster in result) {
+            _clusters += cluster
+            cluster.update()
+            listener.onClusterEvent(cluster, Event.NEW)
+        }
+
+        reorder()
     }
 
     fun reset() {
         _clusters.clear()
     }
 
-    // Uses a rectangle overlap algorithm to return whether the covered area is greater than 1
-    private fun canBePartOfCluster(cluster: Cluster<T>, block: ClusterBlock<T>): Boolean {
-        val x1 = maxOf(cluster.x, block.x) - 1
-        val y1 = maxOf(cluster.y, block.y) - 1
-        val x2 = minOf(cluster.x + cluster.w, block.x + block.size) + 1
-        val y2 = minOf(cluster.y + cluster.h, block.y + block.size) + 1
-        return (x2 - x1) * (y2 - y1) > 1
+    private fun reorder() {
+        _clusters.sortWith(compareBy(Cluster<T>::x, Cluster<T>::y))
+    }
+
+    private fun canBePartOfCluster(cluster: Cluster<T>, block: ClusterBlock<T>): Boolean = cluster._blocks.any {
+        val r1 = it.x + it.size + 1
+        val l1 = it.x - 1
+        val b1 = it.y - 1
+        val t1 = it.y + it.size + 1
+
+        val r2 = block.x + block.size + 1
+        val l2 = block.x - 1
+        val b2 = block.y - 1
+        val t2 = block.y + block.size + 1
+
+        val x1 = maxOf(l1, l2)
+        val y1 = maxOf(b1, b2)
+        val x2 = minOf(r1, r2)
+        val y2 = minOf(t1, t2)
+
+        maxOf(0, x2 - x1) * maxOf(0, y2 - y1) > 4
     }
 
     fun interface Listener<T : Any> {
-        fun onClusterUpdate(cluster: Cluster<T>)
+        fun onClusterEvent(cluster: Cluster<T>, event: Event)
+    }
+
+    enum class Event {
+        NEW, UPDATE, REMOVE
     }
 }
 
-class Cluster<T : Any> {
+class Cluster<T : Any>(blocks: List<ClusterBlock<T>> = emptyList()) {
     var x: Int = 0
         private set
     var y: Int = 0
@@ -94,7 +162,7 @@ class Cluster<T : Any> {
     val blocks: List<ClusterBlock<T>> get() = _blocks
 
     @Suppress("PropertyName")
-    internal val _blocks: MutableList<ClusterBlock<T>> = mutableListOf()
+    internal val _blocks: MutableList<ClusterBlock<T>> = blocks.toMutableList()
 
     fun copy() = Cluster<T>().also {
         it.x = x
@@ -109,6 +177,7 @@ class Cluster<T : Any> {
         y = _blocks.minOf { it.y }
         w = _blocks.maxOf { it.x + it.size } - x
         h = _blocks.maxOf { it.y + it.size } - y
+        _blocks.sortWith(compareBy(ClusterBlock<T>::x, ClusterBlock<T>::y))
     }
 }
 
---
+++ imperium-mindustry/src/test/kotlin/com/xpdustry/imperium/mindustry/misc/BlockClusterManagerTest.kt
@@ -0,0 +1,138 @@
+/*
+ * Imperium, the software collection powering the Xpdustry network.
+ * Copyright (C) 2023  Xpdustry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.xpdustry.imperium.mindustry.misc
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import java.net.InetAddress
+
+class BlockClusterManagerTest {
+
+    @Test
+    fun `test blocks that share a side`() {
+        val manager = createManager()
+        manager.addElement(createBlock(0, 0, 1))
+        manager.addElement(createBlock(1, 0, 1))
+        Assertions.assertEquals(1, manager.clusters.size)
+
+        val cluster = manager.clusters[0]
+        Assertions.assertEquals(2, cluster.blocks.size)
+        Assertions.assertEquals(0, cluster.x)
+        Assertions.assertEquals(0, cluster.y)
+        Assertions.assertEquals(2, cluster.w)
+        Assertions.assertEquals(1, cluster.h)
+    }
+
+    @Test
+    fun `test blocks that do not share a side`() {
+        val manager = createManager()
+        manager.addElement(createBlock(2, 2, 2))
+        manager.addElement(createBlock(-2, 0, 1))
+        manager.addElement(createBlock(10, 10, 10))
+        Assertions.assertEquals(3, manager.clusters.size)
+    }
+
+    @Test
+    fun `test blocks that partially share a side`() {
+        val manager = createManager()
+        manager.addElement(createBlock(1, 1, 2))
+        manager.addElement(createBlock(3, 2, 2))
+        Assertions.assertEquals(1, manager.clusters.size)
+    }
+
+    @Test
+    fun `test blocks that only share a corner`() {
+        val manager = createManager()
+        manager.addElement(createBlock(0, 0, 1))
+        manager.addElement(createBlock(1, 1, 1))
+        Assertions.assertEquals(2, manager.clusters.size)
+    }
+
+    @Test
+    fun `test simple remove 1`() {
+        val manager = createManager()
+        for (x in 0..4) {
+            for (y in 0..4) {
+                manager.addElement(createBlock(x, y, 1))
+            }
+        }
+
+        Assertions.assertEquals(1, manager.clusters.size)
+        Assertions.assertEquals(25, manager.clusters[0].blocks.size)
+
+        // Removes a U shape inside the 5 by 5 square
+        for (x in 1..3) {
+            for (y in 1..3) {
+                if (x == 1 && (y == 1 || y == 2)) continue
+                manager.removeElement(x, y)
+            }
+        }
+
+        Assertions.assertEquals(1, manager.clusters.size)
+        Assertions.assertEquals(18, manager.clusters[0].blocks.size)
+    }
+
+    @Test
+    fun `test simple remove 2`() {
+        val manager = createManager()
+        for (x in 0..2) {
+            for (y in 0..5) {
+                manager.addElement(createBlock(x, y, 1))
+            }
+        }
+
+        Assertions.assertEquals(1, manager.clusters.size)
+        Assertions.assertEquals(18, manager.clusters[0].blocks.size)
+
+        manager.removeElement(0, 1)
+        manager.removeElement(1, 1)
+
+        Assertions.assertEquals(1, manager.clusters.size)
+        Assertions.assertEquals(16, manager.clusters[0].blocks.size)
+    }
+
+    @Test
+    fun `test remove split`() {
+        val manager = createManager()
+        for (x in 0..2) {
+            manager.addElement(createBlock(x, 0, 1))
+        }
+        manager.addElement(createBlock(1, 1, 1))
+        Assertions.assertEquals(1, manager.clusters.size)
+        manager.removeElement(1, 0)
+        Assertions.assertEquals(3, manager.clusters.size)
+    }
+
+    @Test
+    fun `test simple merge`() {
+        val manager = createManager()
+        for (y in 0..2) {
+            for (x in 0..2) {
+                manager.addElement(createBlock(x, y * 2, 1))
+            }
+        }
+        Assertions.assertEquals(3, manager.clusters.size)
+        manager.addElement(createBlock(1, 1, 1))
+        Assertions.assertEquals(2, manager.clusters.size)
+        manager.addElement(createBlock(1, 3, 1))
+        Assertions.assertEquals(1, manager.clusters.size)
+    }
+
+    private fun createManager() = BlockClusterManager<Unit> { _, _ -> }
+    private fun createBlock(x: Int, y: Int, size: Int) = ClusterBlock(x, y, size, InetAddress.getLocalHost(), Unit)
+}
