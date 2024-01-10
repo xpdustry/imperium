f17f90a76dab1b6406643a971e8e0fa3d8fff43d
Jason <131086642+JasonP01@users.noreply.github.com>
2024-01-11 01:07:34 +0200

    More punishing punishments, and a less punishing one

--- imperium-discord/src/main/kotlin/com/xpdustry/imperium/discord/commands/ModerationCommand.kt
+++ imperium-discord/src/main/kotlin/com/xpdustry/imperium/discord/commands/ModerationCommand.kt
@@ -91,7 +91,7 @@ class ModerationCommand(instances: InstanceManager) : ImperiumApplication.Listen
         actor: InteractionSender.Slash,
         player: Snowflake,
         reason: String,
-        duration: PunishmentDuration = PunishmentDuration.THREE_HOURS
+        duration: PunishmentDuration = PunishmentDuration.THREE_DAYS
     ) {
         onPunishCommand("Banned", Punishment.Type.BAN, actor, player, reason, duration.value)
     }
@@ -101,7 +101,7 @@ class ModerationCommand(instances: InstanceManager) : ImperiumApplication.Listen
         actor: InteractionSender.Slash,
         player: Snowflake,
         reason: String,
-        duration: PunishmentDuration = PunishmentDuration.ONE_HOUR
+        duration: PunishmentDuration = PunishmentDuration.ONE_DAY
     ) {
         onPunishCommand("Muted", Punishment.Type.MUTE, actor, player, reason, duration.value)
     }
@@ -151,6 +151,7 @@ class ModerationCommand(instances: InstanceManager) : ImperiumApplication.Listen
 }
 
 enum class PunishmentDuration(val value: Duration) {
+    FIFTEEN_MINUTES(15.minutes),
     ONE_HOUR(1.hours),
     THREE_HOURS(3.hours),
     ONE_DAY(1.days),
