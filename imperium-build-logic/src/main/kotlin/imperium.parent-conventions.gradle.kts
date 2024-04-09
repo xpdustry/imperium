b53f506f0ffff409b9f37a4ea33c969670233856
phinner <62483793+phinner@users.noreply.github.com>
2024-04-09 17:55:20 +0200

    fix(build): Remove ben-manes versions plugin in favor of renovate

--- imperium-build-logic/src/main/kotlin/imperium.parent-conventions.gradle.kts
+++ imperium-build-logic/src/main/kotlin/imperium.parent-conventions.gradle.kts
@@ -1,26 +1,7 @@
-plugins {
-    id("com.github.ben-manes.versions")
-}
-
 tasks.register("incrementVersionFile") {
     doLast { file("VERSION.txt").writeText(project.version.toString()) }
 }
 
 tasks.register("printVersion") {
     doLast { println(project.version.toString()) }
-}
-
-// https://github.com/ben-manes/gradle-versions-plugin
-fun isNonStable(version: String): Boolean {
-    val stableKeyword = listOf("RELEASE", "FINAL", "GA").any { version.uppercase().contains(it) }
-    val regex = "^[0-9,.v-]+(-r)?$".toRegex()
-    val isStable = stableKeyword || regex.matches(version)
-    return isStable.not()
-}
-
-tasks.dependencyUpdates {
-    // https://github.com/ben-manes/gradle-versions-plugin
-    rejectVersionIf {
-        isNonStable(candidate.version) && !isNonStable(currentVersion)
-    }
 }
