66d3c55c1b8678975492e9091f95bc6a4361befc
Jason <131086642+JasonP01@users.noreply.github.com>
2024-04-14 20:25:12 +0200

    second wip
    gotta do some learnin fr fr

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/SpawnUnitCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/SpawnUnitCommand.kt
@@ -1,1 +1,52 @@
-wip
+/*
+ * Imperium, the software collection powering the Xpdustry network.
+ * Copyright (C) 2023  Xpdustry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+package com.xpdustry.imperium.mindustry.world
+
+import arc.math.Mathf
+import com.xpdustry.distributor.annotation.method.EventHandler
+import com.xpdustry.distributor.command.CommandSender
+import com.xpdustry.imperium.common.account.Rank
+import com.xpdustry.imperium.common.application.ImperiumApplication
+import com.xpdustry.imperium.common.async.ImperiumScope
+import com.xpdustry.imperium.common.command.ImperiumCommand
+import com.xpdustry.imperium.common.command.ImperiumPermission
+import com.xpdustry.imperium.common.config.ServerConfig
+import com.xpdustry.imperium.common.content.MindustryGamemode
+import com.xpdustry.imperium.common.inject.InstanceManager
+import com.xpdustry.imperium.common.inject.get
+import com.xpdustry.imperium.mindustry.command.annotation.ClientSide
+import com.xpdustry.imperium.mindustry.game.MenuToPlayEvent
+import com.xpdustry.imperium.mindustry.misc.Entities
+import com.xpdustry.imperium.mindustry.misc.ImmutablePoint
+import com.xpdustry.imperium.mindustry.misc.PlayerMap
+import com.xpdustry.imperium.mindustry.misc.runMindustryThread
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.isActive
+import kotlinx.coroutines.launch
+import mindustry.Vars
+import mindustry.game.EventType
+import mindustry.game.Team
+import mindustry.gen.Call
+import mindustry.gen.Player
+
+class SpawnUnitCommand(instances: InstanceManager) :
+    ImperiumApplication.Listener {
+
+    
+      
+    }
