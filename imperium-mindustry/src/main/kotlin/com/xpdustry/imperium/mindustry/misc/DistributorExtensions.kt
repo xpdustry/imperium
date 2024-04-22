3e0ea6318b00eaa23b8a0e0ef0126e5f252fb370
phinner <62483793+Phinner@users.noreply.github.com>
2024-04-22 16:46:15 +0200

    chore: Update licence header

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/misc/DistributorExtensions.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/misc/DistributorExtensions.kt
@@ -1,6 +1,6 @@
 /*
- * Imperium, the software collection powering the Xpdustry network.
- * Copyright (C) 2023  Xpdustry
+ * Imperium, the software collection powering the Chaotic Neutral network.
+ * Copyright (C) 2024  Xpdustry
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
