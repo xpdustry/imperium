33d66588d3de6156197cc1a1cda7c91fa6a5cf14
Jason <jasoncpayne2007@gmail.com>
2024-04-29 20:16:34 +0200

    chore: Add bundle entry and fix bugs

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/HereCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/HereCommand.kt
@@ -1,17 +1,23 @@
 package com.xpdustry.imperium.mindustry.world
 
+
+import com.xpdustry.distributor.command.CommandSender
 import com.xpdustry.imperium.common.application.ImperiumApplication
 import com.xpdustry.imperium.common.command.ImperiumCommand
 import com.xpdustry.imperium.mindustry.command.annotation.ClientSide
 import mindustry.gen.Call
 
-class HereCommand : ImperiumApplication {
+class HereCommand : ImperiumApplication.Listener {
 
     @ImperiumCommand(["here"])
     @ClientSide
-    fun onHereCommand() (
+    fun onHereCommand(
         sender: CommandSender
     ) {
-        Call.sendMessage("$sender: I am at (${sender.player.x}, ${sender.player.y}).")
+        val x = (sender.player.x / 8).toInt()
+        val y = (sender.player.y / 8).toInt()
+        Call.sendMessage("[[${sender.name}[white]]: I am at ($x, $y).")
     }
 }
+
+// Optionally change this file to pure chat commands file
