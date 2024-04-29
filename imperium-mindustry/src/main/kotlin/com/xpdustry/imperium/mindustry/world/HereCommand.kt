6b190e60e62176964a1f8186bbe069aeacce95cf
JasonP01 <JasonP01@users.noreply.github.com>
2024-04-29 18:34:46 +0000

    chore: Format code

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/HereCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/HereCommand.kt
@@ -1,5 +1,21 @@
+/*
+ * Imperium, the software collection powering the Chaotic Neutral network.
+ * Copyright (C) 2024  Xpdustry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.xpdustry.imperium.mindustry.world
-
 
 import com.xpdustry.distributor.command.CommandSender
 import com.xpdustry.imperium.common.application.ImperiumApplication
@@ -11,9 +27,7 @@ class HereCommand : ImperiumApplication.Listener {
 
     @ImperiumCommand(["here"])
     @ClientSide
-    fun onHereCommand(
-        sender: CommandSender
-    ) {
+    fun onHereCommand(sender: CommandSender) {
         val x = (sender.player.x / 8).toInt()
         val y = (sender.player.y / 8).toInt()
         Call.sendMessage("[[${sender.name}[white]]: I am at ($x, $y).")
