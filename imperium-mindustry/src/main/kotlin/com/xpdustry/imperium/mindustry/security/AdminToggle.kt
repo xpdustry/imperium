a33b1e9b8305739b98ca5c85a3d3f0666ee40b39
JasonP01 <JasonP01@users.noreply.github.com>
2024-05-16 09:06:56 +0000

    chore: Format code

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/security/AdminToggle.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/security/AdminToggle.kt
@@ -1,3 +1,20 @@
+/*
+ * Imperium, the software collection powering the Chaotic Neutral network.
+ * Copyright (C) 2024  Xpdustry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.xpdustry.imperium.mindustry.security
 
 import com.xpdustry.distributor.command.CommandSender
@@ -10,12 +27,11 @@ class AdminToggle : ImperiumApplication.Listener {
 
     @ImperiumCommand(["admin"], Rank.OVERSEER)
     @ClientSide
-    fun onAdminToggleCommand(
-        sender: CommandSender
-        ) {
+    fun onAdminToggleCommand(sender: CommandSender) {
         sender.player.admin = !sender.player.admin
-        sender.player.sendMessage("[accent]Your admin status has been set to ${sender.player.admin}")
-    } 
+        sender.player.sendMessage(
+            "[accent]Your admin status has been set to ${sender.player.admin}")
+    }
 }
 
 // I plan on moving this to a centralised file,
