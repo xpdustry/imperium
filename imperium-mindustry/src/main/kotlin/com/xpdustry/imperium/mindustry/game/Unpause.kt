4986b65f33539679bbd913dd28338b25d012c98e
Jason <131086642+JasonP01@users.noreply.github.com>
2024-07-24 13:08:10 +0200

    feat: unpause command
    Testing required

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/game/Unpause.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/game/Unpause.kt
@@ -3,27 +3,26 @@ import com.xpdustry.imperium.common.application.ImperiumApplication
 import com.xpdustry.imperium.common.command.ImperiumCommand
 import com.xpdustry.imperium.mindustry.command.annotation.ClientSide
 import com.xpdustry.imperium.mindustry.misc.onEvent
+import mindustry.core.GameState
 import mindustry.game.EventType.PlayerJoin
-import mindustry.game.EventType.Trigger
 
 package com.xpdustry.imperium.mindustry.game
 
 class UnpauseCommand : ImperiumApplication.Listener {
 
-  fun joinListener() {
-    onEvent(Trigger.PlayerJoin
-    onEvent<PlayeJoin>
-    
+  onEvent<PlayerJoin> {
+    if (Vars.state.isPaused())
+      Call.sendChatMessage("[lightgray]The server is paused, type [orange]/unpause[lightgray] to unpause the server")
   }
-  
+
   @ImperiumCommand(["unpause"])
   @ClientSide
   fun onUnpauseCommand(sender: CommandSender) {
-
-    if (Vars.state.paused) {
-      Vars.state.paused = false
+    if (Vars.state.isPaused()) {
+      state.set(GameState.State.playing)
+      Call.sendChatMessage("${sender.player.name}[white] unpaused the server using [orange]/unpause")
+    } else {
+      Call.sendMessage("The server is already unpaused")
     }
-
-    Call.sendChatMessage("${sender.player.name}[white] unpaused the server using [orange]/unpause")
   }
 }
