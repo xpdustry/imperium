abac26247370b6ac2951ccf55054363713c01306
phinner <62483793+phinner@users.noreply.github.com>
2025-01-18 02:06:00 +0100

    chore(deps): Update dependencies

--- imperium-common/src/main/kotlin/com/xpdustry/imperium/common/misc/CaffeineExtensions.kt
+++ imperium-common/src/main/kotlin/com/xpdustry/imperium/common/misc/CaffeineExtensions.kt
@@ -25,17 +25,17 @@ import kotlinx.coroutines.async
 import kotlinx.coroutines.future.asCompletableFuture
 import kotlinx.coroutines.future.await
 
-fun <K, V> buildCache(configure: Caffeine<K, V>.() -> Unit): Cache<K, V> {
+fun <K : Any, V : Any> buildCache(configure: Caffeine<K, V>.() -> Unit): Cache<K, V> {
     @Suppress("UNCHECKED_CAST") val builder = Caffeine.newBuilder() as Caffeine<K, V>
     configure(builder)
     return builder.build()
 }
 
-fun <K, V> buildAsyncCache(configure: Caffeine<K, V>.() -> Unit): AsyncCache<K, V> {
+fun <K : Any, V : Any> buildAsyncCache(configure: Caffeine<K, V>.() -> Unit): AsyncCache<K, V> {
     @Suppress("UNCHECKED_CAST") val builder = Caffeine.newBuilder() as Caffeine<K, V>
     configure(builder)
     return builder.buildAsync()
 }
 
-suspend fun <K, V> AsyncCache<K, V>.getSuspending(key: K, compute: suspend (K) -> V): V =
+suspend fun <K : Any, V : Any> AsyncCache<K, V>.getSuspending(key: K, compute: suspend (K) -> V): V =
     get(key) { k, _ -> ImperiumScope.IO.async { compute(k) }.asCompletableFuture() }.await()
