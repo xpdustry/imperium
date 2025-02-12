27302269fa46126db146aa7fafbee7235840ab40
Jason <JasonP01@users.noreply.github.com>
2025-02-12 14:19:34 +0200

    forgot to add color

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/chat/ImperiumPlaceholderProcessor.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/chat/ImperiumPlaceholderProcessor.kt
@@ -139,6 +139,7 @@ class ImperiumPlaceholderProcessor(
             Rank.OVERSEER -> Color.green
             Rank.MODERATOR -> Color.royal
             Rank.ADMIN,
+            Rank.MANAGER,
             Rank.OWNER -> Color.scarlet
         }
 
