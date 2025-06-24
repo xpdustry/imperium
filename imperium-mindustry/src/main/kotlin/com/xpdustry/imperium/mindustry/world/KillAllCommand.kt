4b5702a9ba45b16d39bc34597ee91d69216debc7
phinner <62483793+Phinner@users.noreply.github.com>
2025-06-24 14:05:04 +0200

    chore: Unify things

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/KillAllCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/KillAllCommand.kt
@@ -18,6 +18,12 @@
 package com.xpdustry.imperium.mindustry.world
 
 import com.xpdustry.distributor.api.command.CommandSender
+import com.xpdustry.distributor.api.component.ListComponent.components
+import com.xpdustry.distributor.api.component.NumberComponent.number
+import com.xpdustry.distributor.api.component.TextComponent.space
+import com.xpdustry.distributor.api.component.TranslatableComponent.translatable
+import com.xpdustry.distributor.api.component.style.ComponentColor
+import com.xpdustry.distributor.api.translation.TranslationArguments.array
 import com.xpdustry.imperium.common.account.Rank
 import com.xpdustry.imperium.common.application.ImperiumApplication
 import com.xpdustry.imperium.common.command.ImperiumCommand
@@ -37,6 +43,7 @@ import kotlin.time.Duration.Companion.seconds
 import mindustry.game.Team
 import mindustry.gen.Call
 import mindustry.type.UnitType
+import org.incendo.cloud.annotation.specifier.Range
 
 class KillAllCommand(instances: InstanceManager) :
     AbstractVoteCommand<Unit>(instances.get(), "killall", 30.seconds), ImperiumApplication.Listener {
@@ -69,53 +76,51 @@ class KillAllCommand(instances: InstanceManager) :
         onPlayerCancel(sender.player, manager.session)
     }
 
-    @ImperiumCommand(["killall|ku", "team|t"], Rank.MODERATOR)
+    // TODO: This should have a better name instead of "force"
+    @ImperiumCommand(["killall|ku", "force|f"], Rank.MODERATOR)
     @ClientSide
-    fun onKillUnitsTeamCommand(sender: CommandSender, team: Team, @Flag("u") unittype: UnitType? = null) {
-        var count = 0
-        for (unit in Entities.getUnits().toList()) {
-            if (!unit.isPlayer && unit.controller() !is FormationAI && team == unit.team()) {
-                if (unittype == null) {
-                    Call.unitDespawn(unit)
-                    count++
-                } else if (unit.type == unittype) {
-                    Call.unitDespawn(unit)
-                    count++
-                }
-            }
-        }
-        // TODO: make this translatable
-        sender.reply("Killed $count ${unittype?.localizedName ?: "unit"}(s) from team ${team.coloredName()}")
-    }
-
-    // Kill individual units
-    // TODO: merge with the above command? Make team optional?
-    @ImperiumCommand(["kill|k", "unittype|u"], Rank.MODERATOR)
-    @ClientSide
-    fun onKillUnitsCommand(
+    fun onKillAllUnitsTeamCommand(
         sender: CommandSender,
-        unittype: UnitType,
-        @Flag("c") count: Int? = null,
+        @Flag("u") type: UnitType? = null,
         @Flag("t") team: Team? = null,
+        @Flag("c") @Range(min = "1") count: Int? = null,
     ) {
-        var amount = 0
+        var killed = 0
         for (unit in Entities.getUnits().toList()) {
-            if (amount == count) break
-            if (!unit.isPlayer && unit.controller() !is FormationAI && (team == null || team == unit.team())) {
-                if (unit.type == unittype) {
-                    Call.unitDespawn(unit)
-                    amount++
+            if (
+                !unit.isPlayer &&
+                    (team == null || team == unit.team()) &&
+                    unit.controller() !is FormationAI &&
+                    (type == null || unit.type() == type)
+            ) {
+                killed++
+                Call.unitDespawn(unit)
+                if (count != null && killed >= count) {
+                    break
                 }
             }
         }
-        // TODO: translate this
-        sender.reply(
-            "Killed $amount ${unittype.localizedName}(s) ${if (team != null) "from team ${team.coloredName()}" else ""}"
+
+        val components = components()
+        components.append(
+            translatable("imperium.killall.force.success.base", array(number(killed, ComponentColor.ACCENT)))
         )
+        if (type != null) {
+            components.append(
+                space(),
+                translatable("imperium.killall.force.success.type", array(translatable(type, ComponentColor.ACCENT))),
+            )
+        }
+        if (team != null) {
+            components.append(
+                space(),
+                translatable("imperium.killall.force.success.team", array(translatable(team, ComponentColor.ACCENT))),
+            )
+        }
+        sender.reply(components.build())
     }
 
     override fun getVoteSessionDetails(session: VoteManager.Session<Unit>): String =
-        // TODO: this to
         "Type [orange]/killall <y/n>[] to kill all units."
 
     override suspend fun onVoteSessionSuccess(session: VoteManager.Session<Unit>) {
