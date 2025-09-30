fd5377df8db4f768c5194911d64b65bad5f7bd11
phinner <62483793+Phinner@users.noreply.github.com>
2025-09-30 23:53:13 +0200

    feat(mindustry): Added /item fill

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/ItemCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/ItemCommand.kt
@@ -22,30 +22,70 @@ import com.xpdustry.distributor.api.command.cloud.specifier.AllTeams
 import com.xpdustry.imperium.common.account.Rank
 import com.xpdustry.imperium.common.application.ImperiumApplication
 import com.xpdustry.imperium.common.command.ImperiumCommand
+import com.xpdustry.imperium.mindustry.command.annotation.ClientSide
+import com.xpdustry.imperium.mindustry.command.annotation.ServerSide
 import kotlin.math.absoluteValue
+import kotlin.math.min
+import mindustry.Vars
 import mindustry.game.Team
 import mindustry.type.Item
 
+// TODO Translate this shi
+@Suppress("DuplicatedCode")
 class ItemCommand : ImperiumApplication.Listener {
 
     @ImperiumCommand(["item", "add|a"], Rank.ADMIN)
+    @ClientSide
+    @ServerSide
     fun onItemAddCommand(sender: CommandSender, item: Item, amount: Int, @AllTeams team: Team = sender.player.team()) {
+        if (!Vars.state.isGame) {
+            sender.error("Not playing. Host first.")
+            return
+        }
+
         val core = team.core()
         if (core == null) {
             sender.reply("Could not find a core for team ${team.localized()}.")
             return
         }
 
+        val verb: String
+        val value: Int
         if (amount == 0) {
             sender.reply("Amount must not be zero!")
+            return
         } else if (amount > 0) {
-            core.items.add(item, amount)
+            value = min(amount, core.storageCapacity - core.items.get(item))
+            verb = "Added"
+            core.items.add(item, value)
         } else {
-            core.items.remove(item, amount.absoluteValue)
+            value = min(amount.absoluteValue, core.items.get(item))
+            verb = "Removed"
+            core.items.remove(item, value)
         }
 
-        sender.reply(
-            "${if(amount >= 0) "Added" else "Removed"} ${amount.absoluteValue} ${item.localizedName} to ${team.localized()} core."
-        )
+        sender.reply("$verb ${value.absoluteValue} ${item.localizedName} to ${team.localized()} core.")
+    }
+
+    @ImperiumCommand(["item", "fill|f"], Rank.ADMIN)
+    @ClientSide
+    @ServerSide
+    fun onItemFillCommand(sender: CommandSender, @AllTeams team: Team = sender.player.team()) {
+        if (!Vars.state.isGame) {
+            sender.error("Not playing. Host first.")
+            return
+        }
+
+        val core = team.core()
+        if (core == null) {
+            sender.reply("Could not find a core for team ${team.localized()}.")
+            return
+        }
+
+        for (item in Vars.content.items()) {
+            core.items.set(item, core.storageCapacity)
+        }
+
+        sender.reply("Core filled.")
     }
 }
