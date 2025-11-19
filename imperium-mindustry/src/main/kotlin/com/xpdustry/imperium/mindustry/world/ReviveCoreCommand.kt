51b33c6580a0d2fa27b2a8f1e86e6f1a154efe37
JasonP01 <131086642+JasonP01@users.noreply.github.com>
2025-11-19 02:58:11 +0000

    style: Format code

--- imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/ReviveCoreCommand.kt
+++ imperium-mindustry/src/main/kotlin/com/xpdustry/imperium/mindustry/world/ReviveCoreCommand.kt
@@ -1,3 +1,20 @@
+/*
+ * Imperium, the software collection powering the Chaotic Neutral network.
+ * Copyright (C) 2024  Xpdustry
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
 package com.xpdustry.imperium.mindustry.world
 
 import com.xpdustry.distributor.api.annotation.EventHandler
@@ -60,12 +77,8 @@ class ReviveCoreCommand : ImperiumApplication.Listener {
     fun corepage(page: String, pageNumber: Int, pageCount: Int): Component =
         components(
             WHITE,
-            translatable("imperium.revivecore.pages", TranslationArguments.array(page, pageNumber, pageCount + 1))
+            translatable("imperium.revivecore.pages", TranslationArguments.array(page, pageNumber, pageCount + 1)),
         )
 }
 
-data class CoreTile(
-    val tileX: Int,
-    val tileY: Int,
-    val core: CoreBlock,
-)
+data class CoreTile(val tileX: Int, val tileY: Int, val core: CoreBlock)
